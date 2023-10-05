48ab10328d01d9a9281910bb04ce56b522087e04
Liliana2201 <subinaliliana@gmail.com>
2023-10-05 15:46:02 +0700

    Заблокирована кнопка Next, после того, как пользователь ответит на последний вопрос

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -71,8 +71,8 @@ class MainActivity : AppCompatActivity() {
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         questionTextView.setText(questionTextResId)
-        trueButton.visibility=View.VISIBLE
-        falseButton.visibility=View.VISIBLE
+        trueButton.visibility = View.VISIBLE
+        falseButton.visibility = View.VISIBLE
     }
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
@@ -84,8 +84,12 @@ class MainActivity : AppCompatActivity() {
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
-        trueButton.visibility=View.INVISIBLE
-        falseButton.visibility=View.INVISIBLE
+        trueButton.visibility = View.INVISIBLE
+        falseButton.visibility = View.INVISIBLE
+        if (currentIndex == questionBank.size-1){
+            nextButton.visibility = View.INVISIBLE
+        }
+
     }
 
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
